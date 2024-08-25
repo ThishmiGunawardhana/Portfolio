5f9d4bb390b6eb908f4801546c419c380dbd3b0a
ThishmiGunawardhana <121964919+ThishmiGunawardhana@users.noreply.github.com>
2024-08-25 11:59:11 -0700

    update js file

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new typed(".text", {
-    Strings:["Frontend Developer","Web Developer"],
+    strings: ["Frontend Developer","Web Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
